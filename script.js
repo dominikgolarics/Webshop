c92ed1d04d4d1964a2be3e87fb8a4baaab8f083c
reet <lolparodis@gmail.com>
2024-11-14 08:34:35 +0100

    termekek oldal haladva

--- script.js
+++ script.js
@@ -10,4 +10,275 @@ $(document).ready(function () {
         e.preventDefault();
         window.location.href="termekek.html"
     });
+
+    $("#asd-asd").click(function (e) { 
+        e.preventDefault();
+        $(this).toggle(function () {
+            
+        });
+    });
+
+    //Alapa elrejt
+    $("#kiem-div").hide();
+    $("#alacsony-div").hide();
+    $("#magas-div").hide();
+    //
+
+
+    $("#sorrend-span").click(function (e) { 
+        e.preventDefault();
+        $("#kiem-div").slideToggle(function () {
+        });
+        $("#alacsony-div").slideToggle(function () {
+        });
+        $("#magas-div").slideToggle(function () {
+        });
+    });
+
+
+    function markaFilterOpciok(){
+        var rend_div=document.getElementById("rendezes-markak")
+    
+        for (let i = 0; i < tesztAdat.length; i++) {
+            
+            var div = document.createElement("div")
+            var input = document.createElement("input")
+            var label = document.createElement("label")
+            
+            
+    
+            //Annyit generál amennyi egyedi márka van az adatbázisba
+    
+            input.id="marka"+i+"-checkbox" //betöltött index
+            input.type="checkbox"
+            input.name="filter-marka-checkbox"
+            input.value=tesztAdat[i].marka //betöltött adatokból
+    
+            label.setAttribute("for","marka"+i+"-checkbox")
+            label.innerHTML=tesztAdat[i].marka //adatbázisból
+            label.setAttribute("class","marka-label")
+    
+            div.appendChild(input)
+            div.appendChild(label)
+            div.id="marka"+i
+            
+    
+            rend_div.appendChild(div)
+            $("#marka"+i).hide();
+            $("#marka-span").click(function () { 
+                $("#marka"+i).slideToggle(function () {
+                });
+            });
+            
+        }
+    
+    }
+    markaFilterOpciok()
+
+    function meretFilterOpciok(){
+        var szam=1
+        var szam2=1
+        var meret_div=document.getElementById("rendezes-meret")
+        for (let i = 0; i < (tesztAdat.length/3); i++) {
+    
+            var div = document.createElement("div")
+    
+            for (let j = i * 3; j < (i + 1) * 3; j++) {
+                var input = document.createElement("input")
+    
+                var label = document.createElement("label")
+
+                input.type="checkbox"
+                input.name=tesztAdat[j].meret
+                input.id="meret"+szam
+
+                label.setAttribute("for","meret"+szam)
+                label.innerHTML=tesztAdat[j].meret
+                label.setAttribute("class","meret-label")
+
+                szam++
+
+                div.appendChild(input)
+                div.appendChild(label)
+                
+            }
+            div.id="meret"+szam2+"-checkbox"
+            
+            meret_div.appendChild(div)
+
+            //TODO Valamiért nem működik?
+            // $("#meret"+szam2+"-checkbox").hide();
+            // $("#meret-span").click(function () { 
+            //     $("#meret"+szam2+"-checkbox").slideToggle(function () {
+            //     });
+            // });
+            szam2++
+    
+        }
+    
+    }
+    meretFilterOpciok()
+    
 });
+
+const tesztAdat=[
+    {
+        marka:"Nike",
+        ar:35000,
+        meret:34
+    },
+    {
+        marka:"Adidas",
+        ar:35000,
+        meret:35
+    },
+    {
+        marka:"Puma",
+        ar:35000,
+        meret:36
+    },
+    {
+        marka:"Converse",
+        ar:35000,
+        meret:38
+    },
+    {
+        marka:"NewBalance",
+        ar:35000,
+        meret:39
+    },
+    {
+        marka:"Vans",
+        ar:35000,
+        meret:40
+    },
+]
+
+function arCheckboxEllenorzes() { 
+    const ar_checkbox1 = document.getElementById("ar-checkbox1");
+    const ar_checkbox2 = document.getElementById("ar-checkbox2");
+    const ar_checkbox3 = document.getElementById("ar-checkbox3");
+    const ar_checkbox4 = document.getElementById("ar-checkbox4");
+    const ar_checkbox5 = document.getElementById("ar-checkbox5");
+    const ar_checkbox6 = document.getElementById("ar-checkbox6");
+
+    ar_checkbox1.addEventListener("change", function() {
+        if (ar_checkbox1.checked) {
+            console.log("Checkbox1 is checked");
+            //Aminek történnie kell be X-elés után vagy GLOBÁLIS változó megváltoztatása
+        } else {
+            console.log("Checkbox1 is not checked");
+        }
+    });
+
+    ar_checkbox2.addEventListener("change", function() {
+        if (ar_checkbox2.checked) {
+            
+            console.log("Checkbox2 is checked");
+            //Aminek történnie kell be X-elés után vagy GLOBÁLIS változó megváltoztatása
+        } else {
+            console.log("Checkbox2 is not checked");
+        }
+    });
+
+    ar_checkbox3.addEventListener("change", function() {
+        if (ar_checkbox3.checked) {
+            console.log("Checkbox3 is checked");
+            //Aminek történnie kell be X-elés után vagy GLOBÁLIS változó megváltoztatása
+        } else {
+            console.log("Checkbox3 is not checked");
+        }
+    });
+
+    ar_checkbox4.addEventListener("change", function() {
+        if (ar_checkbox4.checked) {
+            console.log("Checkbox4 is checked");
+            //Aminek történnie kell be X-elés után vagy GLOBÁLIS változó megváltoztatása
+        } else {
+            console.log("Checkbox4 is not checked");
+        }
+    });
+
+    ar_checkbox5.addEventListener("change", function() {
+        if (ar_checkbox5.checked) {
+            console.log("Checkbox5 is checked");
+            //Aminek történnie kell be X-elés után vagy GLOBÁLIS változó megváltoztatása
+        } else {
+            console.log("Checkbox5 is not checked");
+        }
+    });
+
+    ar_checkbox6.addEventListener("change", function() {
+        if (ar_checkbox6.checked) {
+            console.log("Checkbox6 is checked");
+            //Aminek történnie kell be X-elés után vagy GLOBÁLIS változó megváltoztatása
+        } else {
+            console.log("Checkbox6 is not checked");
+        }
+    });
+}
+
+function termekekDarab() { 
+    let selectedValue
+
+    let selectElement = document.getElementById("mennyiseg");
+    selectedValue = selectElement.value;
+    const termekek_lista_div = document.getElementById("termekek-lista")
+    termekek_lista_div.innerHTML="";
+    for (let i = 0; i < (selectedValue/4); i++) {
+        
+        const div_cipo_container=document.createElement("div")
+
+        for (let j = i * 4; j < (i + 1) * 4; j++) {
+            const div_cipo_termek=document.createElement("div")
+            const div_cipo_kep=document.createElement("div")
+            const img=document.createElement("img")
+
+            // ? ideiglenes
+            let src ="img/cipo/21A282-100_Tenis-Asics-Gel-Kayano-5-OG-Masculino-Multicolor-2.jpg" 
+
+            const div_cipo_leiras=document.createElement("div")
+            const h3=document.createElement("h3")
+            const span=document.createElement("span")
+            const h5=document.createElement("h5")
+
+            img.src=src //ideiglenes, majd adatbázisból
+            div_cipo_kep.setAttribute("class","cipo-kep") //* stílus
+            div_cipo_kep.appendChild(img)
+            
+            h3.innerHTML="Cipő márkája"+j //adatbázisból
+            span.innerHTML="Teljes termék név" //adatbázisból
+            h5.innerHTML="696969" //adatbázisból
+            div_cipo_leiras.setAttribute("class","cipo-leiras") //* stílus
+            div_cipo_leiras.appendChild(h3)
+            div_cipo_leiras.appendChild(span)
+            div_cipo_leiras.appendChild(h5)
+
+            div_cipo_termek.setAttribute("class","cipo-termek") //* stílus
+            div_cipo_termek.appendChild(div_cipo_kep)
+            div_cipo_termek.appendChild(div_cipo_leiras)
+
+            div_cipo_container.id="cipo-container"  //* stílus
+            div_cipo_container.appendChild(div_cipo_termek)
+        }
+        
+        termekek_lista_div.append(div_cipo_container)
+    }
+ }
+
+//Meghívás
+termekekDarab()
+arCheckboxEllenorzes()
+//Meghívás
+
+
+
+//TESZT ZÓNA 
+console.log(tesztAdat)
+var xd = document.getElementById("ar-checkbox1");
+console.log(xd.checked)
+
+function teszt(){
+    
+    
+}
---
+++ termekek.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="icon" type="image/x-icon" href="img/menu/favicon.ico" />
+		<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+		/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="style/style.css" />
+		<title>Nile - Termékek</title>
+	</head>
+	<body>
+		<div id="fejlec">
+			<header id="fej">
+				<div id="logo">
+					<h2><a id="logo_link" href="main.html">Nile</a></h2>
+				</div>
+				<ul id="fej_tart">
+					<li><a class="fej_link" href="friss.html">Új kiadás</a></li>
+					<li><a class="fej_link" href="legkel.html">Legkelendőbbek</a></li>
+					<li><a class="fej_link" href="#">Termékek</a></li>
+				</ul>
+				<div id="fej_tool">
+					<img id="kosar" src="img/menu/kosar.png" alt="kosar" />
+					<img id="profil" src="img/menu/icon.png" alt="ikon" />
+				</div>
+			</header>
+		</div>
+
+		<div id="minden-cipo">
+			<div id="cipo-termekek">
+				<div id="filter">
+					<div id="rendezes-darab">
+						<label for="">Darab termék</label>
+						<select onchange="termekekDarab()" name="mennyiseg" id="mennyiseg">
+							<option value="16">16</option>
+							<option value="24">24</option>
+							<option value="40">40</option>
+						</select>
+					</div>
+					<div id="rendezes-sorrend">
+						<span id="sorrend-span" style="font-size: 24px; font-weight: bold;">Rendezés</span>
+						<div id="kiem-div">
+							<input id="kiemelt-radio" name="rendezes-radio" value="Kiemelt" type="radio">
+							<label for="kiemelt-radio">Kiemelt</label>
+						</div>
+						<div id="alacsony-div">
+							<input id="ar-alacsony" name="rendezes-radio" value="Ár (alacsony->magas)" type="radio">
+							<label for="ar-alacsony">Ár (alacsony->magas)</label>
+						</div>
+						<div id="magas-div">
+							<input id="ar-magas" name="rendezes-radio" value="Ár (magas->alacsony)" type="radio">
+							<label for="ar-magas">Ár (magas->alacsony)</label>	
+						</div>
+					</div>
+					<div id="rendezes-markak">
+						<span id="marka-span" style="font-size: 24px; font-weight: bold;">Márkák</span>
+					</div>
+					<div id="rendezes-meret">
+						<span id="meret-span" style="font-size: 24px; font-weight: bold;">Méret</span><br>
+					</div>
+					<div id="rendezes-ar">
+						<span style="font-size: 24px; font-weight: bold;">Ár</span>
+						<div>
+							<input id="ar-checkbox1" type="checkbox">
+							<label for="ar-checkbox1">20 000 Ft alatt</label>
+						</div>
+						<div>
+							<input id="ar-checkbox2" type="checkbox">
+							<label for="ar-checkbox2">20 000 Ft - 40 000 Ft</label>
+						</div>
+						<div>
+							<input id="ar-checkbox3" type="checkbox">
+							<label for="ar-checkbox3">40 000 Ft - 80 000Ft</label>
+						</div>
+						<div>
+							<input id="ar-checkbox4" type="checkbox">
+							<label for="ar-checkbox4">80 000 Ft - 100 000Ft</label>
+						</div>
+						<div>
+							<input id="ar-checkbox5" type="checkbox">
+							<label for="ar-checkbox5">100 000 Ft - 140 000Ft</label>
+						</div>
+						<div>
+							<input id="ar-checkbox6" type="checkbox">
+							<label for="ar-checkbox6">140 000Ft felett</label>
+						</div>
+					</div>
+					<button onclick="teszt()">Teszt</button>
+				</div>
+				<div id="termekek-lista">
+				</div>
+			</div>
+		</div>
+
+		<div id="labfej">
+			<footer></footer>
+		</div>
+		<script src="script.js"></script>
+	</body>
+</html>
