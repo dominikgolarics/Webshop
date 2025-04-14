150e71ef2432a9b4be8d40b8e1980bb1087b2c8b
IamKinim <dominikgolarics@gmail.com>
2025-04-14 09:21:21 +0200

    aprobb dolgok kijavitva

--- termekek.php
+++ termekek.php
@@ -16,24 +16,6 @@
 </head>
 
 <body>
-
-	<div id="fejlec">
-		<header id="fej">
-			<div id="logo">
-				<h2><a id="logo_link" href="index.php">Nile</a></h2>
-			</div>
-			<ul id="fej_tart">
-				<li><a class="fej_link" href="friss.php">Új kiadás</a></li>
-				<li><a class="fej_link" href="legkel.php">Legkelendőbbek</a></li>
-				<li><a class="fej_link" href="#">Termékek</a></li>
-			</ul>
-			<div id="fej_tool">
-				<img id="kosar" src="img/menu/kosar.png" alt="kosar" />
-				<img id="profil" src="img/menu/icon.png" alt="ikon" />
-			</div>
-		</header>
-	</div>
-
 	<div id="minden-cipo">
 		<div id="cipo-termekek">
 			<div id="filter">
