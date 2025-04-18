2461e7e3f068d96b4b7540df26c72951c5b4fc94
reet <lolparodis@gmail.com>
2025-04-18 11:57:39 +0200

    ikonok a lábfejhez belinkelve, kosár lábfej nincs az oldal alján

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<link
 		href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
 		rel="stylesheet" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<title>Nile</title>
 </head>
